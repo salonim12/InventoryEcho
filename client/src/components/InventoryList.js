002d875f6bee28f4e62e87beb2e94b9c75de90fe
evanugarte <you@example.com>
2018-10-18 14:59:22 -0700

    Connected MongoDB to Frontend
    MongoDB has now been integrated into the basic app. Now items can be added and deleted to from the inventory by interacting with the UI.
    Removed uuid, as mongoDB has generated ID's, and added axios to handle various http requests.

--- client/src/components/InventoryList.js
+++ client/src/components/InventoryList.js
@@ -20,13 +20,13 @@ class InventoryList extends Component {
       <Container>
         <ListGroup>
           <TransitionGroup>
-            {items.map(({ id, name }) => (
-              <CSSTransition key={id} timeout={500} classNames="fade">
+            {items.map(({ _id, name }) => (
+              <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
                   <Button
                     className="remove-btn"
                     color="danger"
-                    onClick={this.onDeleteClick.bind(this, id)}
+                    onClick={this.onDeleteClick.bind(this, _id)}
                   >
                     &times;
                   </Button>
