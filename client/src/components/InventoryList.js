cdfde8c1360b5755b782fbce66f173f82087f288
evanugarte <you@example.com>
2018-10-18 08:54:21 -0700

    Implemented Redux
    Set Up Redux Functionality
    
    Added Redux to handle our app's state, and better handle deleting an
    event.
    
    Renamed ShoppingList to InventoryList
    
    Since this is an inventory app, it makes sense to have a view be called
    InventoryList rather than ShoppingList. Therefore, ShoppingList was
    renamed.
    
    Finished Implementing Redux
    
    Redux now handles adding items and deleting items. Now that the
    functionality is there, it can be made use of in the future.

---
+++ client/src/components/InventoryList.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { connect } from "react-redux";
+import { getItems, deleteItem } from "../actions/itemActions";
+import PropTypes from "prop-types";
+
+class InventoryList extends Component {
+  componentDidMount() {
+    this.props.getItems();
+  }
+
+  onDeleteClick = (id) => {
+    this.props.deleteItem(id);
+  };
+
+  render() {
+    const { items } = this.props.item;
+    return (
+      <Container>
+        <ListGroup>
+          <TransitionGroup>
+            {items.map(({ id, name }) => (
+              <CSSTransition key={id} timeout={500} classNames="fade">
+                <ListGroupItem>
+                  <Button
+                    className="remove-btn"
+                    color="danger"
+                    onClick={this.onDeleteClick.bind(this, id)}
+                  >
+                    &times;
+                  </Button>
+                  {name}
+                </ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
+    );
+  }
+}
+
+InventoryList.propTypes = {
+  getItems: PropTypes.func.isRequired,
+  item: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  item: state.item
+});
+
+export default connect(
+  mapStateToProps,
+  { getItems, deleteItem }
+)(InventoryList);
