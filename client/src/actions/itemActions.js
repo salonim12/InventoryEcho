02256f679375bcd91c49c1c668e7ef3899fd4277
evanugarte <you@example.com>
2018-11-09 13:44:38 -0800

    Added Get Request for Search
    We can now send a request to MongoDB to look for an item by name. This
    can be done by sending a request like api/product/search/?name=water

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,10 +1,24 @@
 import axios from "axios";
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from "./types";
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING, SEARCH_ITEMS } from "./types";
 
 export const setItemsLoading = () => {
   return {
     type: ITEMS_LOADING
   };
+};
+
+export const submitSearch = (newSearch) => (dispatch) => {
+  axios
+    .get("/api/product/search/", { params: newSearch })
+    // /api/product/search/?description=searchString
+    // if success then res.data will be the product object we looking for
+    .then(res => {
+      dispatch({
+        type: SEARCH_ITEMS,
+        payload: res.data
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 export const getItems = () => (dispatch) => {
