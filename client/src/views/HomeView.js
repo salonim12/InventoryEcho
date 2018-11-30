9fda7e13aef521f4f822941a0f562187cf187a1e
SangT <tps.sang@gmail.com>
2018-11-29 21:07:32 -0800

    Modified HomePage * Added images for homepage * Modified homepage using reactstrap

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -1,27 +1,29 @@
-import React, { Component } from "react";
-import { Container, Row, Col, Jumbotron } from "reactstrap";
+import React from "react";
+import { UncontrolledCarousel } from "reactstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import one from "./one.jpg";
+import two from "./two.jpg";
+import three from "./three.jpg";
 
+const items = [
+    {
+        src: one,
+        caption: "Slide 1",
+        header: "Team 08 - CPME131"
+    },
+    {
+        src: two,
+        caption: "Slide 2",
+        header: "Team 08 - CPME131"
+    },
+    {
+        src: three,
+        caption: "Slide 3",
+        header: "Team 08 - CPME131"
+    }
+];
 
-class HomeView extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Container>
-          <Row>
-            <Col xs="12">
-              <Jumbotron>
-                <h1 className="text-center">
-                  Welcome to iEcho
-                </h1>
-              </Jumbotron>
-            </Col>
-          </Row>
-        </Container>
-      </React.Fragment>
-    );
-  }
-}
+const HomeView = () => <UncontrolledCarousel items={items} />;
 
 export default HomeView;
